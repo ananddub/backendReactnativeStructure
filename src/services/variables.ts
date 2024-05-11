0149f0c000882293b149a87c975284ad60275088
ananddub <duanand6@gmail.com>
2024-05-11 19:17:22 +0530

    structered way wriiten backend

---
+++ src/services/variables.ts
@@ -0,0 +1,12 @@
+export const databaseID = {
+  // host: "89.117.188.154",
+  // user: "u932299896_eduware",
+  // password: "Webgen@220310",
+  // database: "u932299896_sisdb",
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "u932299896_sisdbApp",
+};
+export const IMAGEADD = "../../uploads";
+export const PORT = 4000;
